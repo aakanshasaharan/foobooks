ff4e927a191b956d05609b331164f623a3cd7e75
Aakansha Saharan <sudheerjawla@Aakanshas-MacBook-Air-2.local>
2015-10-05 19:11:05 -0400

    Undid public directory being a submodule

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,23 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/books', function () {
+    return "Here are all the books in the fooobooks.";
+});
+
+
+Route::get('/bio', function () {
+    return "Here is the bio for all the participants.";
+});
+
+Route::get('/people', function () {
+    return "No of people could go upto 1000.";
+});
+
+
+Route::get('/detail', function () {
+    return "what are the details.";
+});
+
